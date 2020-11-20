bd6c2cfd3800fedc9a32c5c79120988734e0b3b4
dietercaes <47667377+dietercaes@users.noreply.github.com>
2020-11-21 00:34:42 +0100

    opdracht 1 - 7

--- schattenjacht.py
+++ schattenjacht.py
@@ -3,25 +3,34 @@ import base64
 import hashlib
 import urllib.request
 import os
-import json
-from base64 import b64encode
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-url = 'http://185.115.217.205:1234/opdracht2'
+url = 'http://185.115.217.205:1234'
 
-r = requests.post('http://185.115.217.205:1234/opdracht2', json={"nr1": "Eerste regel",
-                                                                 "nr2": "Tweede regel",
-                                                                 "nr3": "Derde regel"})
+# Getting first task.
+
+r = requests.get(url + "/opdracht1")
 result = r.text
-# print(result)
+print(result)
+
+# Opdracht 1
+
+r = requests.post(url + "/opdracht2", json={"nr1": "Eerste regel",
+                                            "nr2": "Tweede regel",
+                                            "nr3": "Derde regel"})
+result = r.text
+print(result)
 
 # Opdracht 2
+
 my_string = "opdracht 3"
 my_bytes = my_string.encode("utf-8")
 # print(my_bytes)
 
-r = requests.post('http://185.115.217.205:1234/opdracht3/' + my_bytes.hex())
+r = requests.post(url + "/opdracht3/" + my_bytes.hex())
+result = r.text
+print(result)
 
 # Opdracht 3
 
@@ -31,10 +40,10 @@ my_bytes = my_string.encode("utf-8")
 # print(base64.b64encode(my_bytes))
 # print(base64.b64encode(my_bytes).decode("utf-8"))
 
-r = requests.post('http://185.115.217.205:1234/opdracht4/' + base64.b64encode(my_bytes).decode("utf-8"))
+r = requests.post(url + "/opdracht4/" + base64.b64encode(my_bytes).decode("utf-8"))
 
 result = r.text
-# print(result)
+print(result)
 
 # Opdracht 4
 
@@ -50,13 +59,14 @@ with open(file, 'rb') as f:  # Open the file to read it's bytes
 
 # print(file_hash.hexdigest())  # Get the hexadecimal digest of the hash
 
-r = requests.post('http://185.115.217.205:1234/opdracht5',
+r = requests.post(url + "/opdracht5",
                   json={"sha512": file_hash.hexdigest(), "relatieve_url": "/static/opdracht4"})
 
 result = r.text
-# print(result)
+print(result)
 
 # Opdracht 5
+
 real_checksum = "5f2a5c31a292cc46bacf546c961a8424"
 
 files = ["/static/opdracht5/applicatie_jos.exe",
@@ -66,43 +76,36 @@ files = ["/static/opdracht5/applicatie_jos.exe",
          "/static/opdracht5/applicatie_mariette.exe",
          "/static/opdracht5/applicatie_ivonne.exe"]
 
-#print('Beginning file download with urllib2...')
-
 for file in files:
-    url = 'http://185.115.217.205:1234' + file
     file_name = file[18:]
-    urllib.request.urlretrieve(url, os.getcwd() + "/" + file_name)
-
+    urllib.request.urlretrieve(url + file,
+                               os.getcwd() + "/" + file_name)  # Download file and store it in current directory.
     hasher = hashlib.md5()
     with open(file_name, 'rb') as open_file:
         content = open_file.read()
         hasher.update(content)
     if hasher.hexdigest() == real_checksum:
         correct_file = file_name
-        #print(file_name)
 
 correct_file_url = "/static/opdracht5/" + correct_file
-r = requests.post('http://185.115.217.205:1234/opdracht6', json={"relatieve_url": correct_file_url})
+r = requests.post(url + "/opdracht6", json={"relatieve_url": correct_file_url})
 result = r.text
-# print(result)
+print(result)
 
 # Opdracht 6
 
-result = r.text
-# print(result)
-
 
 data = b"Geheim bericht bestemd voor de docenten IoT aan de KdG"
-print(data)
+# print(data)
 key = get_random_bytes(32)  # Creating a 256 bit key (32 * 8 = 256)
-print(key.hex())
+# print(key.hex())
 cipher = AES.new(key, AES.MODE_EAX)
 ciphertext, tag = cipher.encrypt_and_digest(data)
-print(ciphertext.hex())
-print(cipher.nonce.hex())
+# print(ciphertext.hex())
+# print(cipher.nonce.hex())
 
-r = requests.post('http://185.115.217.205:1234/opdracht7', json={"bericht_versleuteld": ciphertext.hex(),
-                                                                 "sleutel": key.hex(),
-                                                                 "nonce": cipher.nonce.hex()})
+r = requests.post(url + "/opdracht7", json={"bericht_versleuteld": ciphertext.hex(),
+                                            "sleutel": key.hex(),
+                                            "nonce": cipher.nonce.hex()})
 result = r.text
 print(result)
